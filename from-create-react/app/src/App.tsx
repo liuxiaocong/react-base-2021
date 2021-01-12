7066e6d1acf603bae47d499508911ab9224f630b
Evan Liu <evan.liu@shopee.com>
2021-01-12 11:32:10 +0800

    Add sass support

--- from-create-react/app/src/App.tsx
+++ from-create-react/app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import s from "./s.module.scss";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={s.color}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
